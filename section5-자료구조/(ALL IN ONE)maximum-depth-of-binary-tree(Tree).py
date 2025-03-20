8073ad4937bcd5e5b8e961ffaebdf5da6e898aa1
emilywin825 <emilywin825@naver.com>
2025-03-20 15:54:20 +0900

    (ALL IN ONE)maximum-depth-of-binary-tree(Tree).py

---
+++ section5-자료구조/(ALL IN ONE)maximum-depth-of-binary-tree(Tree).py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/maximum-depth-of-binary-tree/
+class Solution(object):
+    def maxDepth(self, root):
+        max_dept=0
+        if root is None:
+            return max_dept
+        Q=deque()
+        Q.append((root, 1))
+        while Q:
+            now,cur_dept=Q.popleft()
+            max_dept=max(max_dept,cur_dept)
+
+            if now.left:
+                Q.append((now.left,cur_dept+1))
+            if now.right:
+                Q.append((now.right,cur_dept+1))
+
+        return max_dept
+        
