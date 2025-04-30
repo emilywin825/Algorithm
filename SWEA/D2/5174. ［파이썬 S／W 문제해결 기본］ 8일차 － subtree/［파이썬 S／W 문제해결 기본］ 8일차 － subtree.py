637433552c6024998d0b08f622ac2ca2226da0bc
이민정 <102593109+emilywin825@users.noreply.github.com>
2025-04-30 18:41:39 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 8일차 - subtree, Time: 67 ms, Memory: 59,392 KB -BaekjoonHub

--- SWEA/D2/5174. ［파이썬 S／W 문제해결 기본］ 8일차 － subtree/［파이썬 S／W 문제해결 기본］ 8일차 － subtree.py
+++ SWEA/D2/5174. ［파이썬 S／W 문제해결 기본］ 8일차 － subtree/［파이썬 S／W 문제해결 기본］ 8일차 － subtree.py
@@ -3,7 +3,6 @@ for test_case in range(1, T + 1):
     e,n=map(int,input().split())
     lis=list(map(int,input().split()))
     tree={}
-    res=1
 
     for i in range(0,len(lis),+2):
         if lis[i] in tree: 
@@ -14,20 +13,12 @@ for test_case in range(1, T + 1):
             tree[lis[i]]=[lis[i+1]]
     
     def sub_tree(i):
-        global res
-        if i in tree:
-            res+=len(tree[i])
-            for k in tree.get(i):
-                sub_tree(k)
-
-                
-
-    if n not in tree:
-        res=1
-    else:
-        for i in tree.get(n):#루트 자식 가져오고
-            res+=1
-            if i in tree:
-                sub_tree(i)
-
+        if i not in tree:
+            return 1
+        total=1
+        for child in tree[i]:
+            total+=sub_tree(child)
+        return total
+      
+    res=sub_tree(n)
     print(f"#{test_case} {res}")
