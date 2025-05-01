d33308cb2348bf2a8ffa86ef859c348b66b68ed4
이민정 <102593109+emilywin825@users.noreply.github.com>
2025-05-01 14:13:18 +0900

    [Silver V] Title: 나이순 정렬, Time: 2580 ms, Memory: 48188 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -0,0 +1,14 @@
+N = int(input())
+
+# 2xN
+array = []
+
+
+for i in range(N):
+    age, name = input().split()
+    array.append([int(age),name])
+
+array.sort(key=lambda x :x[0])
+
+for i in array:
+    print(i[0],i[1])
