f1db20c4c65db19a61ba322741e71f9c60cba519
이민정 <102593109+emilywin825@users.noreply.github.com>
2025-06-16 00:18:16 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 36 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
@@ -3,9 +3,13 @@ input=sys.stdin.readline
 
 T=int(input())
 dp=[1]*10001
-for i in range(2,4):
-    for j in range(i,10001):
-        dp[j]+=dp[j-i]
+
+for i in range(2,10001):
+    dp[i]+=dp[i-2]
+    
+for i in range(3,10001):
+    dp[i]+=dp[i-3]
+
         
 for _ in range(T):
     n=int(input())
