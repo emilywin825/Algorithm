6af0aae7d1b5644c77d7029959c3e3464187181a
이민정 <102593109+emilywin825@users.noreply.github.com>
2025-06-16 13:36:56 +0900

    [Gold V] Title: 동전, Time: 64 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.py
+++ 백준/Gold/9084. 동전/동전.py
@@ -1,12 +1,14 @@
+import sys
+input=sys.stdin.readline
+
 T=int(input())
 for _ in range(T):
     n=int(input())
-    coin=list(map(int,input().split()))
+    coins=list(map(int,input().split()))
     m=int(input())
     dp=[0]*(m+1)
     dp[0]=1
-    for c in coin:
-        for i in range(m+1):
-            if c<=i:
-                dp[i]+=dp[i-c]
+    for c in coins:
+        for i in range(c,m+1):
+            dp[i]+=dp[i-c]
     print(dp[m])
