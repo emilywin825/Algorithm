eba83a5ed2e9d32d28719fdaec90e65dfeb5a96e
이민정 <102593109+emilywin825@users.noreply.github.com>
2025-06-25 17:50:53 +0900

    [Silver II] Title: 색종이 만들기, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,25 +1,23 @@
-n=int(input())
-paper=[list(map(int,input().split())) for _ in range(n)]
-white=0
-blue=0
-def splitt(x,y,n):
+def dc(x,y,n):
     global white, blue
-    
     color=paper[x][y]
     for i in range(x,x+n):
         for j in range(y,y+n):
             if color!=paper[i][j]:
-                splitt(x,y,n//2)
-                splitt(x,y+n//2,n//2)
-                splitt(x+n//2,y,n//2)
-                splitt(x+n//2,y+n//2,n//2)
+                dc(x,y,n//2)
+                dc(x+n//2,y,n//2)
+                dc(x,y+n//2,n//2)
+                dc(x+n//2,y+n//2,n//2)
                 return
     if color==0:
         white+=1
     else:
         blue+=1
-            
-            
-splitt(0,0,n)
+
+n=int(input())
+paper=[list(map(int,input().split())) for _ in range(n)]   
+white=0
+blue=0
+dc(0,0,n)
 print(white)
 print(blue)
