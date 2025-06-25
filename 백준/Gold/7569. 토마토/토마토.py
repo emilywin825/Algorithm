f834c05030f64caea5fb92282a1a848edb19f05d
이민정 <102593109+emilywin825@users.noreply.github.com>
2025-06-26 00:01:46 +0900

    [Gold V] Title: 토마토, Time: 1952 ms, Memory: 50320 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,43 +1,39 @@
+from collections import deque
 import sys
 input=sys.stdin.readline
-from collections import deque
-
+#위,아래,왼,오,앞,뒤
+dx=[0,0,-1,0,1,0]
+dy=[0,0,0,1,0,-1]
+dh=[1,-1,0,0,0,0]
 def bfs():
-    dx=[-1,0,1,0,0,0]
-    dy=[0,1,0,-1,0,0]
-    dh=[0,0,0,0,1,-1]
     while q:
-        z,x,y=q.popleft()
+        e,y,x=q.popleft()
         for i in range(6):
-            xx=x+dx[i]
-            yy=y+dy[i]
-            zz=z+dh[i]
-            if 0<=xx<n and 0<=yy<m and 0<=zz<h and visited[zz][xx][yy]==0 and tomato[zz][xx][yy]==0:
-                    q.append((zz,xx,yy))
-                    visited[zz][xx][yy]=visited[z][x][y]+1
-                    tomato[zz][xx][yy]=1
-                    
+            ee,yy,xx=e+dh[i],y+dy[i],x+dx[i]
+            if 0<=xx<m and 0<=yy<n and 0<=ee<h and tomato[ee][yy][xx]==0:
+                tomato[ee][yy][xx]=tomato[e][y][x]+1
+                q.append((ee,yy,xx))
+                
+#-----------------------  
+#m=행,n=열,h=높이
 m,n,h=map(int,input().split())
+#1:익토, 0:안악토, -1:토마토x
 tomato=[[list(map(int,input().split())) for _ in range(n)]for _ in range(h)]
-visited=[[[0]*m for _ in range(n)] for _ in range(h)]
 q=deque()
-
-for i in range(h):
-     for j in range(n):
-          for k in range(m):
-               if tomato[i][j][k]==1 and visited[i][j][k]==0:
-                    q.append((i,j,k))
-
-bfs()
-
-maxx=-987654321000
-for tt in tomato:
-    for t in tt:
-        if 0 in t:
-            print(-1)
-            exit(0)
-else:
-    for vv in visited:
-        for v in vv:
-            maxx=max(maxx,max(v))
-    print(maxx)
+num=0
+for k in range(h):
+    for i in range(n):
+        for j in range(m):
+            if tomato[k][i][j]==1:
+                q.append((k,i,j))
+                
+bfs()                
+res=-987654321000               
+for k in range(h):
+    for i in range(n):
+        for j in range(m):
+            if tomato[k][i][j]==0:
+                print(-1)
+                sys.exit()
+            res=max(res,tomato[k][i][j])
+print(res-1)
