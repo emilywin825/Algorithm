4fcdfa470d677d980786730a094576b7cc1cfafa
이민정 <102593109+emilywin825@users.noreply.github.com>
2025-06-26 00:24:50 +0900

    [Silver I] Title: 숨바꼭질, Time: 112 ms, Memory: 41140 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,21 +1,22 @@
+from collections import deque
 import sys
 input=sys.stdin.readline
-from collections import deque
 
-def bfs():
+def bfs(start,num):
+    q=deque()
+    q.append((start,num))
     while q:
-        location,time=q.popleft()
-        if location==k:
-            print(time)
-            return
-        if location not in visited and 0<=location<=100000:
-            visited[location]=True
-            q.append((location-1,time+1))
-            q.append((location+1,time+1))
-            q.append((location*2,time+1))
-
+        x,num=q.popleft()
+        if x==k:
+            print(num)
+            sys.exit()
+        else:
+            if visited[x]==0 and 0<=x<=100000:
+                visited[x]=1
+                q.append((x-1,num+1))
+                q.append((x+1,num+1))
+                q.append((x*2,num+1))
+        
 n,k=map(int,input().split())
-q=deque()
-q.append((n,0))
-visited={}
-bfs()
+visited=[0]*(200001)
+bfs(n,0)
