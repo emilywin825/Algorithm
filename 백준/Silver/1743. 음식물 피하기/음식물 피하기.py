4d1ea9b41dceb006a15879e065ce3c4c1e49bf3c
이민정 <102593109+emilywin825@users.noreply.github.com>
2025-06-26 20:33:46 +0900

    [Silver I] Title: 음식물 피하기, Time: 44 ms, Memory: 34264 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
@@ -5,13 +5,13 @@ sys.setrecursionlimit(10**6)
 #n:행,m:열,쓰레기개수:k
 dx=[-1,0,1,0]
 dy=[0,1,0,-1]
-def dfs(x,y,num):
+def dfs(x,y):
+    num=1
     for i in range(4):
         xx,yy=x+dx[i],y+dy[i]
         if 0<=xx<n and 0<=yy<m and visited[xx][yy]==0 and mapp[xx][yy]==1:
             visited[xx][yy]=1
-            num+=1
-            num=dfs(xx,yy,num)
+            num+=dfs(xx,yy)
     return num
     
 n,m,k=map(int,input().split())
@@ -26,6 +26,6 @@ for i in range(n):
     for j in range(m):   
         if mapp[i][j]==1 and visited[i][j]==0:
             visited[i][j]=1
-            res=max(res,dfs(i,j,1))
+            res=max(res,dfs(i,j))
 
 print(res)
